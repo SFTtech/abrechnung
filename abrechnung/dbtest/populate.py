9f5c4d4462291658a6a3635325280a86ae80cd88
Michael Ensslin <michael@ensslin.cc>
2021-04-10 15:31:22 +0200

    test code: Improve populate.py

--- abrechnung/dbtest/populate.py
+++ abrechnung/dbtest/populate.py
@@ -9,25 +9,44 @@ async def populate(test):
 
     auth3b = await test.fetchval(
         'select * from login_with_password(session := $1, password := $2, username := $3)',
-        'other_test', '123456', 'aaa'
+        'other_test', '123456', 'foo1'
     )
 
     # group1 created by usr1
     grp1 = await test.fetchval(
         'select * from group_create(authtoken := $1, name := $2, description := $3, terms := $4, currency_symbol := $5)',
-        auth1, 'example group', 'this is an example group', '', '€', 
+        auth1, 'cool group', 'the best group', '', '€', 
     )
 
     # group2 created by usr3
     grp2 = await test.fetchval(
         'select * from group_create(authtoken := $1, name := $2, description := $3, terms := $4, currency_symbol := $5)',
-        auth3, 'example group', 'this is an example group', '', '€', 
+        auth3, 'amazing group', 'the bestest group', '', '€', 
     )
 
-    #invite1 = await test.fetchval(
-    #    'select * from group_invite(authtoken := $1, grp := $2, description := $3, single_use := $4)',
-    #    auth1, grp1, 'example invite token', True
-    #)
+    # invite into group1 by usr1
+    invite1 = await test.fetchval(
+        'select * from group_invite_create(authtoken := $1, group_id := $2, description := $3, single_use := $4)',
+        auth1, grp1, 'example invite token', False
+    )
 
-    # unused (TODO: populate more stuff using this)
-    del usr2, auth2, usr3, auth3b, grp2 #, invite1
+    # users accept invite to group1
+    for authtoken in (auth2, auth3):
+        await test.fetchval(
+            'select * from group_join(authtoken := $1, invite_token := $2)',
+            authtoken, invite1
+        )
+
+    # users start editing group1
+    await test.fetchval(
+        'select * from create_change(authtoken := $1, group_id := $2, message := $3)',
+        auth1, grp1, "foo1's change"
+    )
+    await test.fetchval(
+        'select * from create_change(authtoken := $1, group_id := $2, message := $3)',
+        auth2, grp1, "bar2's change"
+    )
+    await test.fetchval(
+        'select * from create_change(authtoken := $1, group_id := $2, message := $3)',
+        auth2, grp1, "bar2's second change" 
+    )
