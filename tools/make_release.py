d662abbda421ba7c27744bd4679c86ff64e18c06
Michael Loipführer <michael.loipfuehrer@stusta.de>
2024-01-03 00:13:17 +0100

    --amend

--- tools/make_release.py
+++ tools/make_release.py
@@ -16,18 +16,21 @@ def parse_args():
 
 
 def _get_next_version(part: str) -> tuple[str, str]:
-    ret = subprocess.run(["bump-my-version", "show", "--format", "json", "--increment", part], capture_output=True)
+    ret = subprocess.run(
+        ["bump-my-version", "show", "--format", "json", "--increment", part], capture_output=True, check=True
+    )
     result = json.loads(ret.stdout)
     return result["current_version"], result["new_version"]
 
 
 def main(part: str, dry_run: bool):
+    if dry_run:
+        print("Performing a dry run ...")
     # print current then prompt for new API compatibility version ranges
-    # call bumpversion
     current_version, next_version = _get_next_version(part)
     print(f"Current Version: {current_version}, Upgrading to version {next_version}")
 
-    bump_my_version_args = ["bump-my-version", "bump", part, "--dry-run", "--no-commit", "--no-tag"]
+    bump_my_version_args = ["bump-my-version", "bump", part, "--no-commit", "--no-tag"]
     if dry_run:
         bump_my_version_args.append("--dry-run")
     subprocess.run(bump_my_version_args, check=True)
@@ -35,10 +38,14 @@ def main(part: str, dry_run: bool):
     app_build_gradle = repo_root / "frontend" / "apps" / "mobile" / "android" / "app" / "build.gradle"
     gradle_content = app_build_gradle.read_text()
     if not dry_run:
-        version_code_match = re.match(".*versionCode (?P<version>\d+)\n.*", gradle_content)
-        code = int(version_code_match.group("version"))
-        gradle_content.replace(f"versionCode {code}", f"versionCode {code + 1}")
-        app_build_gradle.write_text(gradle_content)
+        version_code_matches = re.findall(r"versionCode (?P<version>\d+)", gradle_content)
+        for match in version_code_matches:
+            code = int(match)
+            gradle_content = gradle_content.replace(f"versionCode {code}", f"versionCode {code + 1}")
+            app_build_gradle.write_text(gradle_content)
+
+    print("Do not forget to update the api version compatibilities")
+    print("Do not forget to add a debian changelog entry")
 
     # generated changelog from commits / merges / whatever
     # print current changelog
