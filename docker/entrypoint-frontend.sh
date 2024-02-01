b530fbefa7687d3eb416780088aa4568f16159a5
Ricardo Jerónimo <rmjeronimo@netcabo.pt>
2024-02-01 18:47:28 +0000

    Fixed bug where sed would replace substring matches on every container restart

--- docker/entrypoint-frontend.sh
+++ docker/entrypoint-frontend.sh
@@ -2,6 +2,6 @@
 
 CONF="/etc/nginx/conf.d/default.conf"
 
-[ -n "${ABRECHNUNG_API__HOST}" ] && sed -i "s/api:/${ABRECHNUNG_API__HOST}:/g" "$CONF"
-[ -n "${ABRECHNUNG_API__PORT}" ] && sed -i "s/:8080/:${ABRECHNUNG_API__PORT}/g" "$CONF"
-[ ! -f "/proc/net/if_inet6" ] && sed -i "s/listen \[::\]/#listen \[::\]/g" "$CONF"
+[ -n "${ABRECHNUNG_API__HOST}" ] && sed -i "s/ api:/ ${ABRECHNUNG_API__HOST}:/g" "$CONF"
+[ -n "${ABRECHNUNG_API__PORT}" ] && sed -i "s/:8080;/:${ABRECHNUNG_API__PORT};/g" "$CONF"
+[ ! -f "/proc/net/if_inet6" ] && sed -i "s/ listen \[::\]/ #listen \[::\]/g" "$CONF"
