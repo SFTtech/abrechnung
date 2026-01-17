85e0405ba89b5e644965596c903e982aded26813
Michael Loipführer <michael.loipfuehrer@stusta.de>
2026-01-17 15:21:40 +0100

    refactor(web): fully switch to vitest, add barebones playwright e2e project

---
+++ libs/components/vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { nxViteTsPaths } from "@nx/vite/plugins/nx-tsconfig-paths.plugin";
+import { nxCopyAssetsPlugin } from "@nx/vite/plugins/nx-copy-assets.plugin";
+
+export default defineConfig(() => ({
+    root: __dirname,
+    cacheDir: "../../node_modules/.vite/libs/components",
+    plugins: [react(), nxViteTsPaths(), nxCopyAssetsPlugin(["*.md"])],
+    // Uncomment this if you are using workers.
+    // worker: {
+    //  plugins: [ nxViteTsPaths() ],
+    // },
+    test: {
+        name: "test",
+        watch: false,
+        globals: true,
+        environment: "jsdom",
+        include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
+        reporters: ["default"],
+        coverage: {
+            reportsDirectory: "../../coverage/libs/commponents",
+            provider: "v8" as const,
+        },
+    },
+}));
--- libs/redux/jest.config.ts
+++
@@ -1,9 +0,0 @@
-export default {
-    displayName: "redux",
-    preset: "../../jest.preset.js",
-    transform: {
-        "^.+\\.[tj]sx?$": "ts-jest",
-    },
-    moduleFileExtensions: ["ts", "tsx", "js", "jsx"],
-    coverageDirectory: "../../coverage/libs/redux",
-};
